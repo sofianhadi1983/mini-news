c8ef4a351ce6e8b3effb624057059ac2350161f0
Sofian Hadianto <sofianhadianto@ip-192-168-100-4.ap-southeast-1.compute.internal>
2022-09-03 17:46:04 +0700

    Inisialisasi project

---
+++ src/test/java/com/alterra/miniproject/mininews/MininewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alterra.miniproject.mininews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MininewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
